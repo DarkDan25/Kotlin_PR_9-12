ef4fa09ec28cb412d6e9148c2c310e2493358017
Zyablik <elektrosha88@gmail.com>
2024-12-12 13:22:19 +0300

    Made Drawer fixes in practice 11

--- app/src/main/java/com/zyablik/pr_9_12/MainActivity.kt
+++ app/src/main/java/com/zyablik/pr_9_12/MainActivity.kt
@@ -2,6 +2,7 @@ package com.zyablik.pr_9_12
 
 import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -13,7 +14,6 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
@@ -22,14 +22,12 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExtendedFloatingActionButton
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalDrawerSheet
 import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.rememberBottomSheetScaffoldState
 import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -112,87 +110,90 @@ fun PR9(name: String, surname: String, groupp: String ,modifier: Modifier = Modi
             }
         },
         gesturesEnabled = false
-    ){}
-    Scaffold(
-        topBar = {
-            AppBar(
-                title = "I'm a top bar :D"
-            )
-        },
-        bottomBar = {
-            AppBar(
-                title = "I'm a bottom bar >:D"
-            )
-        },
-        content = {
-                innerPadding ->
-            Modifier.padding(innerPadding)
-            Column(
-                modifier = Modifier
-                    .background(MaterialTheme.colorScheme.background)
-                    .fillMaxHeight(0.9f)
-            ){
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center,
+    ){
+        Scaffold(
+            topBar = {
+                AppBar(
+                    title = "I'm a top bar :D"
+                )
+            },
+            bottomBar = {
+                AppBar(
+                    title = "I'm a bottom bar >:D"
+                )
+            },
+            content = {
+                    innerPadding ->
+                Modifier.padding(innerPadding)
+                Column(
                     modifier = Modifier
-                        .background(Color.Magenta)
-                        .fillMaxWidth()
-                        .fillMaxHeight(0.3f)
-
+                        .background(MaterialTheme.colorScheme.background)
+                        .fillMaxHeight(0.9f)
                 ){
-                    Text(
-                        text = "I'm $name $surname\nI'm a BIRD",
-                        textAlign = TextAlign.Center
-                    )
-                    Text(
-                        text = groupp,
-                    )
-                }
-                Box(
-                    Modifier
-                        .background(Color.Cyan)
-                        .fillMaxHeight(0.5f)
-                        .fillMaxWidth()
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.Center,
+                        modifier = Modifier
+                            .background(Color.Magenta)
+                            .fillMaxWidth()
+                            .fillMaxHeight(0.3f)
 
-                ){
-                    LazyRow {
-                        items(arr){ item ->
-                            Text(
-                                text = item,
-                                Modifier.padding(2.dp),
-                                fontSize = 30.sp
-                            )
+                    ){
+                        Text(
+                            text = "I'm $name $surname\nI'm a BIRD",
+                            textAlign = TextAlign.Center
+                        )
+                        Text(
+                            text = groupp,
+                        )
+                    }
+                    Box(
+                        Modifier
+                            .background(Color.Cyan)
+                            .fillMaxHeight(0.5f)
+                            .fillMaxWidth()
+
+                    ){
+                        LazyRow {
+                            items(arr){ item ->
+                                Text(
+                                    text = item,
+                                    Modifier.padding(2.dp),
+                                    fontSize = 30.sp
+                                )
+                            }
                         }
                     }
-                }
-                Card(
-                    Modifier
-                        .background(Color.Green)
-                        .fillMaxHeight(0.8f)
-                        .fillMaxWidth()
+                    Card(
+                        Modifier
+                            .background(Color.Green)
+                            .fillMaxHeight(0.8f)
+                            .fillMaxWidth()
 
-                ) {
-                    Text(
-                        text = someText.value,
-                        fontSize = 40.sp
-                    )
+                    ) {
+                        Text(
+                            text = someText.value,
+                            fontSize = 40.sp
+                        )
+                    }
                 }
+            },
+            floatingActionButton = {
+                ExtendedFloatingActionButton(
+                    onClick = {
+                        someText.value = "You pushed FAB"
+                        scope.launch {
+                            drawerState.apply {
+                                Log.d("rrr",isClosed.toString())
+                                if (isClosed) open() else close()
+                            }
+                        }
+                    }
+                ) { Text("FAB")}
             }
-        },
-        floatingActionButton = {
-            ExtendedFloatingActionButton(
-                onClick = {
-                    someText.value = "You pushed FAB"
-                    scope.launch {
-                        drawerState.apply {
-                            if (isClosed) open() else close()
-                        }
-                    }
-                }
-            ) { Text("FAB")}
-        }
-    )
+        )
+    }
+
 
 }
 
